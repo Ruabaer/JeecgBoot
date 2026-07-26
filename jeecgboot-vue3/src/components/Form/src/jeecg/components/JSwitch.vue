85d9274cf2da304803ab6e1a9e8cb6c29aebfd7e
ruabaer <lipeng1015@hotmail.com>
2026-07-26 15:25:07 +0800

    feat(components): JSwitch支持右侧Modal说明与FormItem全局垂直中线对齐

--- jeecgboot-vue3/src/components/Form/src/jeecg/components/JSwitch.vue
+++ jeecgboot-vue3/src/components/Form/src/jeecg/components/JSwitch.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="prefixCls">
+  <div :class="[prefixCls, 'inline-flex items-center']" style="height: 32px; line-height: 1;">
     <a-select
       v-if="query"
       v-model:value="state"
@@ -9,7 +9,33 @@
       v-bind="attrs"
       @change="onSelectChange"
     />
-    <a-switch v-else v-model:checked="checked" :disabled="disabled" v-bind="attrs" @change="onSwitchChange" />
+    <template v-else>
+      <a-switch v-model:checked="checked" :disabled="disabled" v-bind="attrs" @change="onSwitchChange" />
+      <span
+        v-if="rightLabel || showRightLabel"
+        class="ml-2 select-none text-gray-600 inline-flex items-center leading-none"
+        :class="{ 'cursor-pointer hover:text-primary': helpModalTitle || helpModalContent }"
+        @click="handleHelpClick"
+      >
+        {{ rightLabel || (checked ? labelOptions[0] : labelOptions[1]) }}
+      </span>
+
+      <!-- 问号图标：当配置了 helpModalTitle/helpModalContent 时显示橙色问号，点击调起说明对话框 -->
+      <Icon
+        v-if="helpModalTitle || helpModalContent"
+        icon="ant-design:question-circle-outlined"
+        class="ml-1.5 cursor-pointer text-base inline-flex items-center"
+        style="color: #fa8c16;"
+        title="点击查看规则说明"
+        @click.stop="handleHelpClick"
+      />
+      <BasicHelp
+        v-else-if="helpMessage"
+        class="ml-1 inline-flex items-center"
+        :text="helpMessage"
+        v-bind="helpComponentProps"
+      />
+    </template>
   </div>
 </template>
 
@@ -19,10 +45,14 @@
   import { useAttrs } from '/@/hooks/core/useAttrs';
   import { useDesign } from '/@/hooks/web/useDesign';
   import { useRuleFormItem } from '/@/hooks/component/useFormItem';
+  import { BasicHelp } from '/@/components/Basic';
+  import { Icon } from '/@/components/Icon';
+  import { useMessage } from '/@/hooks/web/useMessage';
+
   const { prefixCls } = useDesign('j-switch');
   const props = defineProps({
     // v-model:value
-    value: propTypes.oneOfType([propTypes.string, propTypes.number]),
+    value: propTypes.oneOfType([propTypes.string, propTypes.number, propTypes.bool]),
     // 取值 options
     options: propTypes.array.def(() => ['Y', 'N']),
     // 文本 options
@@ -31,6 +61,18 @@
     query: propTypes.bool.def(false),
     // 是否禁用
     disabled: propTypes.bool.def(false),
+    // 右侧静态文本 Label
+    rightLabel: propTypes.string.def(''),
+    // 是否在右侧显示动态状态文本（如 “是/否”）
+    showRightLabel: propTypes.bool.def(false),
+    // 弹窗说明标题
+    helpModalTitle: propTypes.string.def(''),
+    // 弹窗说明内容
+    helpModalContent: propTypes.any.def(null),
+    // 右侧绑定帮助提示内容
+    helpMessage: propTypes.oneOfType([propTypes.string, propTypes.array]).def(''),
+    // 帮助提示组件的附加属性
+    helpComponentProps: propTypes.object.def(() => ({ placement: 'top', maxWidth: '480px' })),
   });
   const attrs = useAttrs();
   const emit = defineEmits(['change', 'update:value']);
@@ -74,6 +116,20 @@
     emit('change', value);
     emit('update:value', value);
   }
+
+  function handleHelpClick() {
+    if (props.helpModalTitle || props.helpModalContent) {
+      const { createInfoModal } = useMessage();
+      createInfoModal({
+        title: props.helpModalTitle || '作用说明',
+        width: 520,
+        content: () =>
+          typeof props.helpModalContent === 'function'
+            ? props.helpModalContent()
+            : props.helpModalContent || props.helpMessage,
+      });
+    }
+  }
 </script>
 
 <style lang="less">
@@ -81,5 +137,13 @@
   @prefix-cls: ~'@{namespace}-j-switch';
 
   .@{prefix-cls} {
+    display: inline-flex;
+    align-items: center;
+    line-height: 1;
+
+    .ant-switch {
+      margin-top: 0;
+      margin-bottom: 0;
+    }
   }
 </style>
